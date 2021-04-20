c53c6ad2083af4dd718787129123ad0733d10cc2
yahiro.uchida <yahiro.uchida@monstar-lab.com>
2021-04-20 23:08:22 +0900

    initial commit

---
+++ Chapter1/list0101_1.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+import tkinter
+
+root = tkinter.Tk()
+root.geometry("400x200")
+root.title("PythonでGUIを扱う")
+label = tkinter.Label(root, text="ゲーム開発の一歩", font=("Times New Roman", 20))
+label.place(x=80, y=60)
+root.mainloop()
